495a6575ce7fc6b0bde15dacb06d506ac88a6697
Sowjanya Bysani <Sowjanya_Bysani@epam.com>
2019-04-16 17:25:59 +0530

    worked on reviews

--- app.js
+++ app.js
@@ -1,56 +1,38 @@
 class News {
     articleList;
-    
-    constructor() {
-
-    }
-    
-//    getChannelList(){
-//     var channelList = [{name: 'CNN'},{name:'bbc-news'}]
-//     var fragment = document.createDocumentFragment();
-//     var channel = document.createElement('li');
-//     for (var i = 0; i < channelList.length; i++) {
-//         console.log(channelList[i].name);
-//         channel.innerHTML =  `<a onclick="news.getArticleList('${channelList[i].name}')">CNN</a>`; // news-channel-list
-//        fragment.appendChild(channel);
-//     }
-//     document.getElementById('NewsChannelsList').appendChild(fragment);
-//    }
+    constructor() { }
 
     getArticleList(channelName) {
-        //articleList= "";
-        fetch('https://newsapi.org/v1/articles?source=' + channelName + '&apiKey=b93fd4308dbf445b8a8cdca1f1213172')
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (articleList) {
-                var fragment = document.createDocumentFragment();
-                console.log(JSON.stringify(articleList.articles));
-                //var newsItem = document.createElement('section');
-                for (var i = 0; i < articleList.articles.length; i++) {
-                    var newsItem = document.createElement('section');
-                    document.getElementById("NewsItemsList").innerHTML = "";
-                   newsItem.innerHTML = `
-                    <img
-                        src="${articleList.articles[i].urlToImage}">
-                    <h3 class="title">${articleList.articles[i].title}</h3>
-                    <p class="description">${articleList.articles[i].description}</p>
-                    <em class="foot">-${articleList.articles[i].author}</em>
-                    `;
-                    newsItem.classList.add('news-item');
-                   
-                    fragment.appendChild(newsItem);
-                }
-                
-                document.getElementById('NewsItemsList').appendChild(fragment);
-               
-                return articleList;
-                
+        const apiName = `https://newsapi.org/v1/articles?source=${channelName}&apiKey=b93fd4308dbf445b8a8cdca1f1213172`;
+
+        fetch(apiName)
+            .then(response => response.json())
+            .then(articleList => {
+                this.createNewsFragment(articleList);
             });
     }
 
+    createNewsFragment(articleList) {
+        var fragment = document.createDocumentFragment();
+        for (let i = 0; i < articleList.articles.length; i++) {
+            var newsItem = document.createElement('section');
+            document.getElementById("NewsItemsList").innerHTML = "";
+            newsItem.innerHTML = `
+                <img
+                    src="${articleList.articles[i].urlToImage}">
+                <h3 class="title">${articleList.articles[i].title}</h3>
+                <p class="description">${articleList.articles[i].description}</p>
+                <em class="foot">-${articleList.articles[i].author}</em>
+                `;
+            newsItem.classList.add('news-item');
+            fragment.appendChild(newsItem);
+
+        }
+        document.getElementById('NewsItemsList').appendChild(fragment);
+    }
+
 }
-// document.getElementById('NewsItemsList').appendChild(fragment);  add  
+
 const news = new News();
 
 
